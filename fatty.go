c7bd7c366d2a8934df754a9d7d5d5a24dbc76d7f
pupizoid <pupizoid77@gmail.com>
2016-08-05 12:03:01 +0300

    app redesign

--- fatty.go
+++ fatty.go
@@ -5,66 +5,85 @@ package main
 import (
 	"net/http"
 	"math/rand"
-	"log"
 	"fmt"
 	"encoding/base64"
-	"os"
 	"bytes"
 	"io/ioutil"
-	"flag"
+	"github.com/mkideal/cli"
+	"github.com/c2h5oh/datasize"
+	"bitbucket.org/taruti/mimemagic"
+	clix "github.com/mkideal/cli/ext"
+	"os"
+	"log"
+	"time"
 )
 
 type (
-	Strategy interface {
+	TestingEntity interface {
 		Next()
 		SetBody(*http.Request)
 		SetHeader(*http.Request)
 		GetCurrentSize() int
 		GetPrevSize() int
+		CurrentInfo() string
 	}
 
 	GrovingHeader struct {
-		_type string
+		strategy string
 		ratio int
 		size  int
 	}
 
 	GrovingBody struct {
-		_type string
+		strategy string
 		ratio int
 		size  int
 	}
-	
-	MixedGrovingTest struct {
-		_type string
-		ratio int
-		size  int
+
+	FileBody struct {
+		file clix.File
+		size int
+		mime string
 	}
-	
+
 	TestEntity struct {
-		_type string
+		strategy string
 		ratio int
 		size  int
 	}
 
 	TestingOptions struct {
-		Ratio       int
-		Strategy    Strategy
-		Method      string
-		Dest        string
-		Proxy       ProxySettings
-		MaxRequests int
-		InitialSize int
-		Verbose     bool
+		cli.Helper
+		Multiplier   int    `cli:"m,multi" usage:"sets payload multiplier" dft:"2"`
+		Strategy     string `cli:"*s,strategy" usage:"strategy of payload groving (linear, exponential)"`
+		Entites      []TestingEntity
+		Method       string `cli:"*x,method" usage:"request method to use"`
+		Dest         string `cli:"*d,dest" usage:"destination of requests"`
+		Proxy        string `cli:"proxy" usage:"addr of proxy server to use"`
+		ProxyUsr     string `cli:"proxy-user" usage:"username for proxy-authenticate"`
+		ProxyPwd     string `cli:"proxy-pwd" usage:"password of proxy user"`
+		RequestLimit int    `cli:"l,limit" usage:"limit of requests"`
+		HeaderSize   readableSize `cli:"header" usage:"if specified adds header with given initial value size" dft:"0" parser:"datasize.UnmarchalText"`
+		BodySize     readableSize `cli:"b,body" usage:"if specified adds random payload with given initial size" dft:"0" parser:"datasize.UnmarchalText"`
+		FileContent  clix.File `cli:"p,payload-file" usage:"overrides random payload of body by file contents"`
+		Verbose      bool   `cli:"verbose" usage:"verbose flag"`
 	}
 
 	ProxySettings struct {
 		URL, User, Pass string
 	}
+
+	readableSize struct {
+		size datasize.ByteSize
+	}
 )
 
-func (ps *ProxySettings) getAuthField() string {
-	return base64.StdEncoding.EncodeToString([]byte(ps.User + ":" + ps.Pass))
+func (rs *readableSize) Decode(s string) error {
+	return rs.size.UnmarshalText([]byte(s))
+}
+
+func (rs readableSize) NotNull() bool {
+	return rs.size != 0x0
 }
 
 func getRandomValue(size int) []byte {
@@ -74,10 +93,6 @@ func getRandomValue(size int) []byte {
 		b[i] = letterRunes[rand.Intn(len(letterRunes))]
 	}
 	return b
-}
-
-func NewGrovingHeader(start_size, ratio int, _type string) *GrovingHeader {
-	return &GrovingHeader{ratio: ratio, size: start_size, _type: _type}
 }
 
 // Body is nil for groving header strategy
@@ -92,27 +107,27 @@ func (gh *GrovingHeader) GetCurrentSize() int {
 }
 
 func (gh *GrovingHeader) GetPrevSize() int {
-	var payload int 
-	switch gh._type {
+	var payload int
+	switch gh.strategy {
 	case "linear":
-	 	payload = gh.size - gh.ratio
-	case "expo":
+		payload = gh.size - gh.ratio
+	case "exponential":
 		payload = gh.size / gh.ratio
 	}
 	return payload
 }
 
 func (gh *GrovingHeader) Next() {
-	switch gh._type {
+	switch gh.strategy {
 	case "linear":
 		gh.size += gh.ratio
-	case "expo":
+	case "exponential":
 		gh.size = gh.size * gh.ratio
 	}
 }
 
-func NewGrovingBody(start_size, ratio int, _type string) *GrovingBody {
-	return &GrovingBody{ratio: ratio, size: start_size, _type: _type}
+func (gh *GrovingHeader) CurrentInfo() string {
+	return fmt.Sprintf("Header testing size - %d", gh.GetCurrentSize())
 }
 
 func (gb *GrovingBody) SetBody(req *http.Request) {
@@ -126,59 +141,47 @@ func (gb *GrovingBody) GetCurrentSize() int {
 }
 
 func (gb *GrovingBody) GetPrevSize() int {
-	var payload int 
-	switch gb._type {
+	var payload int
+	switch gb.strategy {
 	case "linear":
-	 	payload = gb.size - gb.ratio
-	case "expo":
+		payload = gb.size - gb.ratio
+	case "exponential":
 		payload = gb.size / gb.ratio
 	}
 	return payload
 }
 
 func (gb *GrovingBody) Next() {
-	switch gb._type {
+	switch gb.strategy {
 	case "linear":
 		gb.size += gb.ratio
-	case "expo":
+	case "exponential":
 		gb.size = gb.size * gb.ratio
 	}
 }
 
-func NewMixedGrovingTest(ratio, start_size int, _type string) *MixedGrovingTest {
-	return &MixedGrovingTest{ratio: ratio, size: start_size, _type: _type}
+func (gb *GrovingBody) CurrentInfo() string {
+	return fmt.Sprintf("Body testing size - %d", gb.GetCurrentSize())
 }
 
-func (mg *MixedGrovingTest) SetBody(req *http.Request) {
-	req.Body = ioutil.NopCloser(bytes.NewReader(getRandomValue(mg.size)))
+func (fb *FileBody) SetBody(req *http.Request) {
+	req.Body = ioutil.NopCloser(bytes.NewReader(fb.file.Data()))
 }
 
-func (mg *MixedGrovingTest) SetHeader(req *http.Request) {
-	req.Header.Set("X-Groving-Header", string(getRandomValue(mg.size)))
-}
-
-func (mg *MixedGrovingTest) GetCurrentSize() int {
-	return mg.size
-}
-
-func (mg *MixedGrovingTest) GetPrevSize() int {
-	var payload int 
-	switch mg._type {
-	case "linear":
-	 	payload = mg.size - mg.ratio
-	case "expo":
-		payload = mg.size / mg.ratio
+func (fb *FileBody) SetHeader(req *http.Request) {
+	if fb.mime != "" {
+		req.Header.Add("Content-Type", fb.mime)
 	}
-	return payload
 }
 
-func (mg *MixedGrovingTest) Next() {
-	switch mg._type {
-	case "linear":
-		mg.size += mg.ratio
-	case "expo":
-		mg.size = mg.size * mg.ratio
-	}
+func (fb *FileBody) GetCurrentSize() int { return fb.size }
+
+func (fb *FileBody) GetPrevSize() int { return fb.size }
+
+func (fb *FileBody) Next() {}
+
+func (fb *FileBody) CurrentInfo() string {
+	return fmt.Sprintf("Body testing size - %d, mime type - %s", fb.GetCurrentSize(), fb.mime)
 }
 
 func processRequest(c *http.Client, options *TestingOptions) {
@@ -186,19 +189,28 @@ func processRequest(c *http.Client, options *TestingOptions) {
 	if err != nil {
 		log.Fatalf("Error creating request to the server: %v\nProgram terminated\n", err)
 	}
-	if options.Proxy.User != "" && options.Proxy.Pass != "" {
-		req.Header.Add("Proxy-Authorization", fmt.Sprintf("Basic %s", options.Proxy.getAuthField()))
+	if options.ProxyUsr != "" && options.ProxyPwd != "" {
+		req.Header.Add("Proxy-Authorization", fmt.Sprintf(
+			"Basic %s", base64.StdEncoding.EncodeToString([]byte(options.ProxyUsr + ":" + options.ProxyPwd))))
 	}
 	// apply payload
-	options.Strategy.SetHeader(req)
-	options.Strategy.SetBody(req)
+	for _, entity := range options.Entites {
+		entity.SetHeader(req)
+		entity.SetBody(req)
+		//if options.Verbose {
+		//	log.Printf("setting request data by %#v\n", entity)
+		//}
+	}
 	// checks
-	switch options.Strategy.(type) {
-	case *GrovingBody:
-		if options.Method == "HEAD" || options.Method == "GET" {
-			log.Fatal("Testing body size not allowed for this method")
+	for _, entity := range options.Entites {
+		switch entity.(type) {
+		case *GrovingBody:
+			if options.Method == "HEAD" || options.Method == "GET" {
+				log.Fatal("Testing body size not allowed for this method")
+			}
 		}
 	}
+
 	//
 	resp, err := c.Do(req)
 	if err != nil {
@@ -206,78 +218,142 @@ func processRequest(c *http.Client, options *TestingOptions) {
 	}
 	if resp.StatusCode != http.StatusOK {
 		log.Printf("Received %d request status code, terminating...\n", resp.StatusCode)
-		log.Printf("Testing size - %d\n", options.Strategy.GetCurrentSize())
-		log.Printf("Last successful testing size - %d\n", options.Strategy.GetPrevSize())
+		for _, entity := range options.Entites {
+			log.Println(entity.CurrentInfo())
+		}
+		//log.Printf("Testing size - %d\n", options.strategy.GetCurrentSize())
+		//log.Printf("Last successful testing size - %d\n", options.strategy.GetPrevSize())
 		os.Exit(0)
 	} else if options.Verbose {
-		log.Printf("Successful request, testing size - %d", options.Strategy.GetCurrentSize())
+		//log.Printf("Successful request, testing size - %d", options.strategy.GetCurrentSize())
+		log.Print("Successful request. ")
+		for _, entity := range options.Entites {
+			log.Print(entity.CurrentInfo())
+		}
+		log.Print("\n")
 	}
 	resp.Body.Close()
-	options.Strategy.Next()
+	for _, entity := range options.Entites {
+		entity.Next()
+	}
 }
 
 func main() {
-	options := new(TestingOptions)
-	flag.StringVar(&options.Dest, "dest", "", "Destination of a testing server")
-	flag.StringVar(&options.Method, "method", "", "Request method")
-	flag.StringVar(&options.Proxy.URL, "proxy", "", "Destination of a proxy server")
-	flag.StringVar(&options.Proxy.User, "proxy-user", "", "Proxy username")
-	flag.StringVar(&options.Proxy.Pass, "proxy-pass", "", "Proxy password")
-	flag.IntVar(&options.Ratio, "r", 2, "Testing value increase ratio")
-	flag.IntVar(&options.InitialSize, "i", 1, "Testing value initial size")
-	flag.IntVar(&options.MaxRequests, "m", 0, "Max number of requests to perform [0 = limitless]")
-	flag.BoolVar(&options.Verbose, "v", false, "Verbosive output")
 
-	var (
-		testHeader, testBody bool
-		strategy string
-	)
-	flag.BoolVar(&testHeader, "h", false, "Test header size")
-	flag.BoolVar(&testBody, "b", false, "Test body size")
-	flag.StringVar(&strategy, "strategy", "", "Testing value groving strategy [linear,expo]")
-
-	flag.Parse()
-
-	if strategy != "linear" && strategy != "expo" {
-		log.Fatal("Unsupported testing strategy")
-	}
-	if testBody && !testHeader {
-		log.Println("Testing request Body with groving size:")
-		options.Strategy = &GrovingBody{
-			ratio: options.Ratio,
-			size: options.InitialSize,
-			_type: strategy,
+	cli.Run(new(TestingOptions), func(ctx *cli.Context) error {
+		options := ctx.Argv().(*TestingOptions)
+		// check strategy
+		if !(options.Strategy == "linear" || options.Strategy == "exponential") {
+			return fmt.Errorf("%s: only 'linear' or 'exponential' supported.", ctx.Color().Red("Err"))
 		}
-	} else if !testBody && testHeader {
-		log.Println("Testing request Header with groving size:")
-		options.Strategy = &GrovingHeader{
-			ratio: options.Ratio,
-			size: options.InitialSize,
-			_type: strategy,
-		}
-	} else if testBody && testHeader {
-		log.Println("Testing request Body and Header with groving size:")
-		options.Strategy = &MixedGrovingTest{
-			ratio: options.Ratio,
-			size: options.InitialSize,
-			_type: strategy,
-		}
-	} else {
-		log.Fatal("Specify entity to test (header,body or both)")
-	}
 
-	client := &http.Client{Transport: &http.Transport{Proxy: http.ProxyFromEnvironment}}
-	if options.Proxy.URL != "" {
-		os.Setenv("HTTP_PROXY", options.Proxy.URL)
-	}
-
-	reqCount := 0
-	for {
-		processRequest(client, options)
-		reqCount++
-		if options.MaxRequests != 0 && options.MaxRequests < reqCount {
-			log.Printf("Successfuly made %d requests", reqCount)
-			os.Exit(0)
+		if options.BodySize.NotNull() && options.FileContent.String() == "" {
+			options.Entites = append(options.Entites, &GrovingBody{strategy: options.Strategy,
+				ratio: options.Multiplier, size: int(options.BodySize.size.Bytes())})
+		} else if options.FileContent.String() != "" {
+			options.Verbose = true
+			options.RequestLimit = 1
+			fileBody := &FileBody{
+				file: options.FileContent,
+				mime: mimemagic.Match(options.FileContent.String(), options.FileContent.Data()),
+				size: len(options.FileContent.Data()),
+			}
+			options.Entites = append(options.Entites, fileBody)
 		}
-	}
+
+		if options.HeaderSize.NotNull() {
+			options.Entites = append(options.Entites, &GrovingHeader{strategy: options.Strategy,
+				ratio: options.Multiplier, size: int(options.HeaderSize.size.Bytes())})
+		}
+
+		if len(options.Entites) == 0 {
+			return fmt.Errorf("%s: no testing entity specified, please set body or header size", ctx.Color().Red("Err"))
+		}
+
+		//fmt.Printf("%#v\n", options)
+
+		client := &http.Client{Transport: &http.Transport{Proxy: http.ProxyFromEnvironment}, Timeout: time.Second * 10}
+		if options.Proxy != "" {
+			os.Setenv("HTTP_PROXY", options.Proxy)
+		}
+
+		reqCount := 0
+		for {
+			processRequest(client, options)
+			reqCount++
+			if options.RequestLimit != 0 && options.RequestLimit <= reqCount {
+				log.Printf("Successfuly made %d requests", reqCount)
+				os.Exit(0)
+			}
+		}
+
+		return nil
+	})
+
+
+
+
+
+	//flag.StringVar(&options.Dest, "dest", "", "Destination of a testing server")
+	//flag.StringVar(&options.Method, "method", "", "Request method")
+	//flag.StringVar(&options.Proxy.URL, "proxy", "", "Destination of a proxy server")
+	//flag.StringVar(&options.Proxy.User, "proxy-user", "", "Proxy username")
+	//flag.StringVar(&options.Proxy.Pass, "proxy-pass", "", "Proxy password")
+	//flag.IntVar(&options.Ratio, "r", 2, "Testing value increase ratio")
+	//flag.IntVar(&options.InitialSize, "i", 1, "Testing value initial size")
+	//flag.IntVar(&options.MaxRequests, "m", 0, "Max number of requests to perform [0 = limitless]")
+	//flag.BoolVar(&options.Verbose, "v", false, "Verbosive output")
+	//
+	//var (
+	//	testHeader, testBody int
+	//	strategy string
+	//)
+	//flag.IntVar(&testHeader, "h", false, "Test header size")
+	//flag.IntVar(&testBody, "b", false, "Test body size")
+	//flag.StringVar(&strategy, "strategy", "", "Testing value groving strategy [linear,expo]")
+	//
+	//flag.Parse()
+	//
+	//if strategy != "linear" && strategy != "expo" {
+	//	log.Fatal("Unsupported testing strategy")
+	//}
+	//if testBody && !testHeader {
+	//	log.Println("Testing request Body with groving size:")
+	//	options.Strategy = &GrovingBody{
+	//		ratio: options.Ratio,
+	//		size: options.InitialSize,
+	//		strategy: strategy,
+	//	}
+	//} else if !testBody && testHeader {
+	//	log.Println("Testing request Header with groving size:")
+	//	options.Strategy = &GrovingHeader{
+	//		ratio: options.Ratio,
+	//		size: options.InitialSize,
+	//		strategy: strategy,
+	//	}
+	//} else if testBody && testHeader {
+	//	log.Println("Testing request Body and Header with groving size:")
+	//	options.Strategy = &MixedGrovingTest{
+	//		ratio: options.Ratio,
+	//		size: options.InitialSize,
+	//		strategy: strategy,
+	//	}
+	//} else {
+	//	log.Fatal("Specify entity to test (header,body or both)")
+	//}
+	//
+	//client := &http.Client{Transport: &http.Transport{Proxy: http.ProxyFromEnvironment}}
+	//if options.Proxy.URL != "" {
+	//	os.Setenv("HTTP_PROXY", options.Proxy.URL)
+	//}
+	//
+	//reqCount := 0
+	//for {
+	//	processRequest(client, options)
+	//	reqCount++
+	//	if options.MaxRequests != 0 && options.MaxRequests < reqCount {
+	//		log.Printf("Successfuly made %d requests", reqCount)
+	//		os.Exit(0)
+	//	}
+	//}
 }
